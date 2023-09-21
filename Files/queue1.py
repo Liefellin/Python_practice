1afbcdd54d4505899f12b49ff600edc541cd0146
Zala <40443395+Liefellin@users.noreply.github.com>
2023-09-21 19:58:23 -0400

    Wrote Stack_pop_count.py and queue1.py named so because if I name it queue the debugger doesn't work??? encountered problems.

--- Files/queue1.py
+++ Files/queue1.py
@@ -1,5 +1,7 @@
 class QueueError(IndexError):  # Choose base class for the new exception.
-    pass
+    def __init__(self):
+        super().__init__()
+        print("I caught it")
 
 
 class Queue:
@@ -10,15 +12,23 @@ class Queue:
         self.queue.insert(0, elem)
 
     def get(self):
-        val = self.queue[-1]
-        del self.queue[-1]
-        return val
+        try:
+            val = self.queue[-1]
+            del self.queue[-1]
+            return val
+        except IndexError:
+            raise QueueError()
+
+    def queue_contents(self):
+        return bool(self.queue)
 
 
 que = Queue()
+print(que.queue_contents())
 que.put(1)
 que.put("dog")
 que.put(False)
+print(que.queue_contents())
 try:
     for i in range(4):
         print(que.get())
