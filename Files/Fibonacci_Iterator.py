c7364f735711e1c9ac8aa50fc2fddb62d10c9ca1
Zala <40443395+Liefellin@users.noreply.github.com>
2023-10-21 17:52:16 -0400

    Completed Fibonacci_Iterator.py

---
+++ Files/Fibonacci_Iterator.py
@@ -0,0 +1,25 @@
+class Fib:
+    def __init__(self, end, quo=0, spark=1):
+        print("__init__")
+        self.__n = end
+        self.__i = 0
+        self.__p1 = quo
+        self.__p2 = spark
+
+    def __iter__(self):
+        print("__iter__")
+        return self
+
+    def __next__(self):
+        print("__next__")
+        self.__i += 1
+        if self.__i > self.__n:
+            raise StopIteration
+        r = self.__p1
+        self.__p1 = self.__p2
+        self.__p2 = self.__p1 +r
+        return r
+
+
+for i in Fib(10, 1, 3):
+    print(i)
